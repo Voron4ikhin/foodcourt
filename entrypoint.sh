c4a432e010779264e5d4b3047565dcafe1b99b3e
Damn_Imposter <33062172+Impo5ter@users.noreply.github.com>
2021-05-16 02:19:58 +0300

    Docker working

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-if ["$DATABASE"="postgres"]
+if [ "$DATABASE" = "postgres" ]
 then
-  echo "Waiting for progress..."
+    echo "Waiting for postgres..."
 
-  while ! nc -z $SQL_HOST $SQL_PORT; do
-    sleep 0.1
-  done
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
 
-  echo "PostgresSQL started"
+    echo "PostgreSQL started"
 fi
 
 python manage.py flush --no-input
